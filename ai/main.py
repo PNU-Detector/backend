2d8f1e57e7703f6711c230840c06e81fd406bbf7
yunzae <dsa05037@naver.com>
2023-09-03 03:30:28 +0900

    feat: Complete building detecting server
    Complete building detecting server using fastapi framework

---
+++ ai/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI ,UploadFile
+from ultralytics import YOLO
+app = FastAPI()
+model= YOLO('building.pt')
+# 이미지 전처리 함수 정의
+
+@app.post("/detect/")
+async def predict(file: UploadFile):
+    try:
+        #이미지를 임시파일로 저장
+        image_path = "temp_image.jpg"
+        with open(image_path, "wb") as temp_image:
+            temp_image.write(file.file.read())
+
+        result = model.predict(source=image_path)[0]
+        # todo no detection인 경우에 예외처리해줘야 함
+        buildingName= model.names[int(result.boxes.cls[0])]
+
+        return {"label": buildingName}
+    except Exception as e:
+        return {"error": str(e)}
